b941fe980304be5004a8ad00123cf1b27d71b8b1
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2022-11-06 21:10:11 +0200

    Adjust archive name

--- create_archive.py
+++ create_archive.py
@@ -2,5 +2,5 @@ import shutil
 from datetime import date
 
 
-shutil.make_archive("PES 2021 Featured Players' Minifaces - " +
+shutil.make_archive("PES 2021 Featured Players' Minifaces " +
                     str(date.today()) + " Update", 'zip', "temp")
