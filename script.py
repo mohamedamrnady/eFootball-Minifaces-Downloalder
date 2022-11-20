3b4f3bac77a4c741990de6acc56f539c0b5c3034
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2022-11-20 14:34:50 +0200

    Add national teams option

--- script.py
+++ script.py
@@ -10,7 +10,11 @@ leagues_urls = league_info_scrapper(
 leagues_names = league_info_scrapper(
     'https://www.pesmaster.com/efootball-2022/', 'name', 2022)
 
+done_players = []
 print('Loaded!')
+
+national_names = []
+national_urls = []
 
 for counter, league_url in enumerate(leagues_urls):
     if leagues_names[counter].find('National') == -1:
@@ -20,6 +24,28 @@ for counter, league_url in enumerate(leagues_urls):
         for team_url in teams_urls:
             players_urls = players_in_team(team_url)
             for player_url in players_urls:
-                miniface_downloader(player_url)
+                try:
+                    done_players.index(player_url)
+                except:
+                    miniface_downloader(player_url)
+                    done_players.append(player_url)
         png_to_dds()
         sort_all_images(leagues_names[counter])
+    else:
+        national_names.append(leagues_names[counter])
+        national_urls.append(league_url)
+
+for counter, national_url in enumerate(national_urls):
+    print('Started Nation(' + str(counter + 1) + '/' +
+          str(len(national_urls)) + ') : ' + national_names[counter])
+    teams_urls = teams_urls_scrapper(national_url)
+    for team_url in teams_urls:
+        players_urls = players_in_team(team_url)
+        for player_url in players_urls:
+            try:
+                done_players.index(player_url)
+            except:
+                miniface_downloader(player_url)
+                done_players.append(player_url)
+    png_to_dds()
+    sort_all_images(national_names[counter])
