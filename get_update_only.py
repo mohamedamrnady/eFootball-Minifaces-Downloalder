24f07a798d1e5ba43abeb77e62259323e9cc2709
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2023-01-01 12:11:14 +0200

    update last thursday function

--- get_update_only.py
+++ get_update_only.py
@@ -7,14 +7,31 @@ def get_last_thursday_date():
     thursday = 3
     today = date.weekday(date.today())
     day = int(str(date.today()).split("-")[2])
+    month = int(str(date.today()).split("-")[1])
+    year = int(str(date.today()).split("-")[0])
     if today < 3:
         thursday = day - today - 4
     if today > 3:
         thursday = day - today + 3
+    if thursday < 0:
+        month -= 1
+        if month == 0:
+            month = 12
+            year -= 1
+        if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+            thursday += 31
+        if month == 4 or month == 6 or month == 9 or month == 11:
+            thursday += 30
+        if month == 2:
+            if (year / 4).denominator != 1:
+                thursday += 28
+            if (year / 4).denominator == 1:
+                thursday += 29
     if thursday < 10:
         thursday = str(0) + str(thursday)
-    return str(date.today()).split(
-        "-")[0] + "-" + str(date.today()).split("-")[1] + "-" + str(thursday)
+    if month < 10:
+        month = str(0) + str(thursday)
+    return f"{str(year)}-{str(month)}-{str(thursday)}"
 
 
 url = 'https://www.pesmaster.com/efootball-2022/player/featured/?date=' + \
