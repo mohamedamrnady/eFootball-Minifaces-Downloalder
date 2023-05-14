785a07ebc6789fb187d7fbcd8b272dac4250d326
Nady <mohamed.amr.nes@gmail.com>
2023-05-14 19:47:23 +0300

    handle unavilable 'Similar Players' section

--- get_miniface.py
+++ get_miniface.py
@@ -14,7 +14,7 @@ def miniface_downloader(url):
     cards_div = soup.find_all(
         'div', attrs={'class': 'player-card-container'})
     try:
-        cards_div = cards_div[1].find_all(
+        cards_div = cards_div[len(cards_div) - 1].find_all(
             'figure', attrs={'class': 'player-card efootball-2022'})
         for cards in cards_div:
             pictures_div = cards.find_all('img')
