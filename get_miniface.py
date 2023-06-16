94769dd75521296371b601c0f46aa168f26cf022
Nady <mohamed.amr.nes@gmail.com>
2023-06-16 14:24:47 +0300

    teams: exclude teams without legends

--- get_miniface.py
+++ get_miniface.py
@@ -3,31 +3,37 @@ from bs4 import BeautifulSoup as bs
 from players_in_team import *
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
 
 
 def miniface_downloader(url):
     all_pictures = []
     r = requests.get(url, headers=headers)
-    soup = bs(r.content, 'html.parser')
+    soup = bs(r.content, "html.parser")
 
-    cards_div = soup.find_all(
-        'div', attrs={'class': 'player-card-container'})
+    cards_div = soup.find_all("div", attrs={"class": "player-card-container"})
+
     try:
         cards_div = cards_div[len(cards_div) - 1].find_all(
-            'figure', attrs={'class': 'player-card efootball-2022'})
+            "figure", attrs={"class": "player-card efootball-2022"}
+        )
         for cards in cards_div:
-            pictures_div = cards.find_all('img')
+            pictures_div = cards.find_all("img")
             for pictures in pictures_div:
-                if pictures['data-src'].find('Variation2022') != -1:
-                    picture_url = 'https://www.pesmaster.com' + \
-                        pictures['data-src']
-                    picture_name = str(picture_url.split(
-                        '/Variation2022/')[1].split('/')[0])
-                    if picture_name.find('b') == -1 and picture_name.find('dummy') == -1:
+                if pictures["data-src"].find("Variation2022") != -1:
+                    picture_url = "https://www.pesmaster.com" + pictures["data-src"]
+                    picture_name = str(
+                        picture_url.split("/Variation2022/")[1].split("/")[0]
+                    )
+                    if (
+                        picture_name.find("b") == -1
+                        and picture_name.find("dummy") == -1
+                    ):
                         all_pictures.append(picture_url)
         if len(all_pictures) != 0:
-            open(str(url.split('/player/')[1].split('/')[0]) + '.png', "wb").write(
-                requests.get(all_pictures[len(all_pictures) - 1]).content)
+            open(str(url.split("/player/")[1].split("/")[0]) + ".png", "wb").write(
+                requests.get(all_pictures[len(all_pictures) - 1]).content
+            )
     except:
         pass
--- players_in_team.py
+++ players_in_team.py
@@ -1,22 +1,22 @@
-from os import cpu_count
 import requests
 from bs4 import BeautifulSoup as bs
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
 
 
 def players_in_team(url):
     all_players = []
 
     r = requests.get(url, headers=headers)
-    soup = bs(r.content, 'html.parser')
-    players_div = soup.find_all(
-        'div', attrs={'class': 'player-card-container'})
-    for counter, players in enumerate(players_div):
-        if counter != 4:
-            players_url = players.find_all('a')
-            for player_url in players_url:
-                player_url = player_url.get_attribute_list('href')[0]
-                all_players.append('https://www.pesmaster.com' + player_url)
+    soup = bs(r.content, "html.parser")
+    players_div = soup.find_all("div", attrs={"class": "player-card-container"})
+    if len(players_div) == 5:
+        for counter, players in enumerate(players_div):
+            if counter != 4:
+                players_url = players.find_all("a")
+                for player_url in players_url:
+                    player_url = player_url.get_attribute_list("href")[0]
+                    all_players.append("https://www.pesmaster.com" + player_url)
     return all_players
