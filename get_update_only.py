eff9f613830b7f8740874353d8a7301535593ba7
Nady <mohamed.amr.nes@gmail.com>
2023-11-18 14:36:57 +0200

    update: set isUpdate to True

--- get_update_only.py
+++ get_update_only.py
@@ -5,6 +5,6 @@ import requests
 
 url = requests.get("https://www.pesmaster.com/efootball-2022/player/featured/").url
 for player_url in players_in_update(url):
-    miniface_downloader(player_url)
+    miniface_downloader(player_url, True)
     png_to_dds()
     sort_all_images(url.split("date=")[1])
