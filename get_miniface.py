ec0eed67fcc70fd84420de19253cbc25c2f45a44
Nady <mohamed.amr.nes@gmail.com>
2024-01-31 14:20:38 +0200

    Remove enum

--- get_miniface.py
+++ get_miniface.py
@@ -1,8 +1,7 @@
-import enum
+import os
 import requests
 from bs4 import BeautifulSoup as bs
 from wand import image
-import os
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
