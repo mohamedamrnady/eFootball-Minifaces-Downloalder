05d004db911584c5d8fc191911f07dc32a85f17c
Nady <mohamed.amr.nes@gmail.com>
2024-01-31 14:40:19 +0200

    update: change work directory

--- get_update_only.py
+++ get_update_only.py
@@ -1,6 +1,11 @@
+import os
 import requests
 from get_miniface import miniface_downloader
 from teams import players_in_update
+
+if not os.path.exists("temp"):
+    os.makedirs("temp")
+os.chdir("temp")
 
 url = requests.get("https://www.pesmaster.com/efootball-2022/player/featured/").url
 for player_url in players_in_update(url):
