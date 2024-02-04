837a210108a530485b2fb0ee838a8ff058443b4d
Nady <mohamed.amr.nes@gmail.com>
2024-02-04 14:58:35 +0200

    CleanUp: Remove unused functions

--- create_archive.py
+++
@@ -1,6 +0,0 @@
-import shutil
-from datetime import date
-
-
-shutil.make_archive("PES 2021 Featured Players' Minifaces " +
-                    str(date.today()) + " Update", 'zip', "temp")
--- create_livecpk_lines.py
+++
@@ -1,9 +0,0 @@
-import os
-
-names = os.listdir('temp/Minifaces')
-
-with open('temp/sider.ini.txt', 'wb') as f:
-    for name in names:
-        f.write(str.encode('cpk.root = ".\livecpk\Minifaces\\' + name + '"'))
-        f.write(b'\n')
-    f.close()
