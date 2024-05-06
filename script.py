bd7b675ba2f4762d48317ba5362908e8c4a6edf9
Nady <mohamed.amr.nes@gmail.com>
2024-05-06 14:45:55 +0300

    Merge National Teams to normal leagues
    * we are already downloading every single face

--- script.py
+++ script.py
@@ -19,40 +19,16 @@ leagues_names = league_info_scrapper(
 done_players = []
 print("Loaded!")
 
-national_names = []
-national_urls = []
-
 for counter, league_url in enumerate(leagues_urls):
-    if leagues_names[counter].find("National") == -1:
-        print(
-            "Started League ("
-            + str(counter + 1)
-            + "/"
-            + str(len(leagues_urls))
-            + ") : "
-            + leagues_names[counter]
-        )
-        teams_urls = teams_urls_scrapper(league_url)
-        for team_url in teams_urls:
-            players_urls = players_in_team(team_url)
-            for player_url in players_urls:
-                if not player_url in done_players:
-                    miniface_downloader(player_url)
-                    done_players.append(player_url)
-    else:
-        national_names.append(leagues_names[counter])
-        national_urls.append(league_url)
-
-for counter, national_url in enumerate(national_urls):
     print(
-        "Started Nation ("
+        "Started League ("
         + str(counter + 1)
         + "/"
-        + str(len(national_urls))
+        + str(len(leagues_urls))
         + ") : "
-        + national_names[counter]
+        + leagues_names[counter]
     )
-    teams_urls = teams_urls_scrapper(national_url)
+    teams_urls = teams_urls_scrapper(league_url)
     for team_url in teams_urls:
         players_urls = players_in_team(team_url)
         for player_url in players_urls:
