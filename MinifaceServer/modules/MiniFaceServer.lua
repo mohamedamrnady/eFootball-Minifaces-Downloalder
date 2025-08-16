d92cdddcf80835c0374666f70087c00bd992b81b
Nady <mohamed.amr.nes@gmail.com>
2025-08-16 20:52:22 +0300

    module: use fs instead of text docs

--- MinifaceServer/modules/MiniFaceServer.lua
+++ MinifaceServer/modules/MiniFaceServer.lua
@@ -6,91 +6,82 @@
 -- originally posted on evo-web & github
 
 local fileroot = ".\\content\\miniface-server"
-local map_teams
-local final_file
 
-local function has_value(tab, val)
+local function has_a_or_b(tab, a, b)
 	for index, value in ipairs(tab) do
-		if value == val then
-			return true
+		if value == a or value == b then
+			return index
 		end
 	end
 	return false
 end
 
+local function tableIsEmpty(self)
+	for _, _ in pairs(self) do
+		return false
+	end
+	return true
+end
+
+local function get_folders_in_dir(dir)
+	local t = {}
+	for name, type in fs.find_files(dir) do
+		if type == "dir" and name ~= "." and name ~= ".." then
+			table.insert(t, tostring(name))
+		end
+	end
+	return t
+end
+
+local function get_files_in_dir(dir, ext)
+	local t = {}
+	local pattern = ext and "%." .. ext .. "$" or "*"
+	for name, type in fs.find_files(dir) do
+		if type == "file" and name:match(pattern) then
+			table.insert(t, tostring(name))
+		end
+	end
+	return t
+end
+
 local function make_key(ctx, filename)
 	if string.match(filename, "common\\render\\symbol\\player") then
-		local home = tostring(ctx.home_team)
-		local away = tostring(ctx.away_team)
+		local home          = tostring(ctx.home_team)
+		local away          = tostring(ctx.away_team)
 
 		local miniface_file = filename:match("^.+[/\\](.+)$")
-		local player_id = miniface_file:sub(0, #miniface_file - 4)
-		local teams = {}
+		local player_id     = miniface_file:sub(0, #miniface_file - 4)
 
-		map_teams = io.open(string.format("%s\\%s\\%s", fileroot, player_id, "map_teams.csv"), "r")
-		if map_teams then
-			for i in map_teams:lines() do
-				table.insert(teams, tostring(i))
-			end
-			map_teams:close()
-			if has_value(teams, home) or has_value(teams, away) then
-				for i, team_id in ipairs(teams) do
-					-- if team_id == current_team_loading then
-					if team_id == home or team_id == away then
-						local map_ids =
-							io.open(string.format("%s\\%s\\%s\\%s", fileroot, player_id, team_id, "map_ids.csv"), "r")
-						if map_ids then
-							math.randomseed(os.time())
-							local minifaces = {}
-							for team in map_ids:lines() do
-								table.insert(minifaces, team)
-							end
-							final_file = string.format(
-								"%s\\%s\\%s",
-								player_id,
-								team_id,
-								minifaces[math.random(1, #minifaces)] .. ".dds"
-							)
-							map_ids:close()
-							return final_file
-						else
-							final_file = filename
-						end
-					end
-				end
-			else
-				local team_id = teams[math.random(1, #teams)]
-				local map_ids =
-					io.open(string.format("%s\\%s\\%s\\%s", fileroot, player_id, team_id, "map_ids.csv"), "r")
-				if map_ids then
-					math.randomseed(os.time())
-					local minifaces = {}
-					for team in map_ids:lines() do
-						table.insert(minifaces, team)
-					end
-					final_file =
-						string.format("%s\\%s\\%s", player_id, team_id, minifaces[math.random(1, #minifaces)] .. ".dds")
-					return final_file
-				else
-					final_file = filename
-				end
-			end
-		else
-			final_file = filename
+		local teams         = get_folders_in_dir(string.format("%s\\%s\\*", fileroot, player_id))
+		if tableIsEmpty(teams) then
+			return filename
 		end
-		return final_file
+		-- log(("Found teams for player %s: %s"):format(player_id, table.concat(teams, ", ")))
+
+		math.randomseed(os.time())
+		local index = has_a_or_b(teams, home, away) or teams[math.random(1, #teams)]
+
+		local minifaces =
+			get_files_in_dir(string.format("%s\\%s\\%s\\*", fileroot, player_id, teams[index]), "dds")
+		if tableIsEmpty(minifaces) then
+			return filename
+		end
+		-- log(("Found minifaces for player %s, team %s: %s"):format(player_id, teams[index], table.concat(minifaces, ", ")))
+
+		return string.format(
+			"%s\\%s\\%s\\%s",
+			fileroot,
+			player_id,
+			teams[index],
+			minifaces[math.random(1, #minifaces)]
+		)
 	end
 end
 
 local function get_filepath(ctx, filename, key)
-	if key and map_teams and string.match(filename, "common\\render\\symbol\\player") then
-		return string.format("%s\\%s", fileroot, final_file)
+	if key then
+		return key
 	end
-end
-
-local function set_teams()
-	map_teams = nil
-	final_file = nil
 end
 
 local function init(ctx)
