4f815778afacbf4d395d6df35d877feb29f23cb5
Nady <mohamed.amr.nes@gmail.com>
2026-06-09 19:29:08 +0300

    fix: assign non-default nationals to default ids

--- check_acl.py
+++ check_acl.py
@@ -1,11 +1,11 @@
 def checkACLID(teamID: str):
     if int(teamID) > 0x4000 and int(teamID) <= 0xC000:  # from ACL eFootball to ACL PES
         return str(int(teamID) + 65536 - 16384)  # + PES 21 ACL OFFSET - eF ACL OFFSET
+    elif int(teamID) > 0x14000:
+        return str(int(teamID) - 0x14000)
     # elif (
     #     int(teamID) > 0xC000 and int(teamID) <= 0x10000
     # ):  # dismiss Asian Comp1 (in case we needed to do another tests)
-    #     return 0
-    # elif int(teamID) > 0x10000:  # dismiss Asian Comp2
     #     return 0
     else:
         return teamID
